ff4f15a2ed2d16b95310a25bf7f9beac566f9c6b
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-01-21 10:47:42 -1000

    1-8 complete
    1-8

--- SwiftPlayground1.playground/Contents.swift
+++ SwiftPlayground1.playground/Contents.swift
@@ -7,9 +7,24 @@ var myFirstVariable: String
 myFirstVariable = "I've created my first string"
 
 //Declare and assign varible in single ine of code
-var mySecondVariable = "and look I added two strings together"
+var mySecondVariable = " and look I added two strings together"
 
 
 var myThirdVarible = myFirstVariable + mySecondVariable
 
 print(myThirdVarible)
+
+var str2 = "Regi's PlayGround"
+
+var regisFirstVarible: String
+
+
+regisFirstVarible = " All I Do is Win "
+
+//Declare and assign varible in single ling of code
+var regisSecondVarible = " And Win All The Time "
+
+
+var regisThirdVarible = regisFirstVarible + regisSecondVarible
+
+print(regisThirdVarible)
