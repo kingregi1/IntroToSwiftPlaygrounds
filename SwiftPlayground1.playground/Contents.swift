9731c5d8ce6cabe79a8c0963e896b1c5de7cd1ee
kingregi1 <59853870+kingregi1@users.noreply.github.com>
2020-01-21 10:24:35 -1000

    Push Files Class 3

--- SwiftPlayground1.playground/Contents.swift
+++ SwiftPlayground1.playground/Contents.swift
@@ -1,3 +1,15 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myFirstVariable: String
+
+myFirstVariable = "I've created my first string"
+
+//Declare and assign varible in single ine of code
+var mySecondVariable = "and look I added two strings together"
+
+
+var myThirdVarible = myFirstVariable + mySecondVariable
+
+print(myThirdVarible)
